410c5c4f42a2d3fb45009c52fa10292339eabcf7
Shubhangi <shubhangitaneja@users.noreply.github.com>
2018-05-23 23:41:35 -0700

    Added reverse words.

---
+++ reverse-words.js
@@ -0,0 +1,16 @@
+/**
+ * Given a string, you need to reverse the order of characters in each word within a sentence while still preserving
+ * whitespace and initial word order.
+ * Example 1:
+ * Input: "Let's take LeetCode contest"
+ * Output: "s'teL ekat edoCteeL tsetnoc"
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    const finalArray = s.split(' ').map((str) => str.split('').reverse().join(''));
+    return finalArray.join(' ');
+};
