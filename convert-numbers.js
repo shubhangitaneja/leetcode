7d0dcfac5373bdb540ba8aa754a0e96d67459d14
Shubhangi Taneja <shubhangi.taneja09@gmail.com>
2018-07-06 20:40:41 -0700

    Adds another implementation for converting roman to integer.

--- convert-numbers.js
+++ convert-numbers.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
+var romanToInt1 = function(s) {
     const specialSymbols = {'I': ['V','X'], 'X': ['L','C'], 'C': ['M', 'D']};
     const symbolValueMap = {
         'I': 1,
@@ -27,6 +27,34 @@ var romanToInt = function(s) {
             i += 1;
         }
         num += symbolValueMap[currentSymbol];
+    }
+    return num;
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt2 = function(s) {
+    const symbolValueMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    };
+    let num = 0;
+    let i = 0;
+    while (i < s.length) {
+        if (symbolValueMap[s[i]] < symbolValueMap[s[i+1]]) {
+            num += symbolValueMap[s[i+1]] - symbolValueMap[s[i]];
+            i += 2; // Skip next character.
+        } else {
+            num += symbolValueMap[s[i]];
+            i += 1;
+        }
     }
     return num;
 };
